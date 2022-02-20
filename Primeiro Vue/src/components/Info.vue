72c7e4058f36b9244e4c9d65e7a663ea1497be3b
Victor Luck <viktor_carvvalho@hotmail.com>
2022-02-20 17:14:52 -0300

    alterações da aula 9 - diretivas vue v-if, v-else, v-show

--- Primeiro Vue/src/components/Info.vue
+++ Primeiro Vue/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Não estou trabalhando</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>PHP</li>
@@ -8,6 +9,7 @@
             <li>VBA</li>
             <li>MYSQL</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
@@ -16,7 +18,9 @@ export default {
     name:'Info',
     data() {
         return {
-            
+            esta_trabalhando : false,
+            mostrar_email: false,
+            email: 'victor.luck'
         }
     }
 }
